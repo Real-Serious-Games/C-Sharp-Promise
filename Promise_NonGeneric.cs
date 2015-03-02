43b22f6f8c94914083b718248740ae4f1cc4ab0e
Ashley Davis <ashley.davis@realseriousgames.com>
2015-03-02 10:33:55 +1000

    Factored out general C# code into a new library: RSG.Toolkit.

--- Promise_NonGeneric.cs
+++ Promise_NonGeneric.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RSG.Utils;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
--- Tests/PromiseTests.cs
+++ Tests/PromiseTests.cs
@@ -1,5 +1,6 @@
 ﻿using Moq;
 using RSG.Promise;
+using RSG.Utils;
 using System;
 using System.Collections.Generic;
 using System.Linq;
