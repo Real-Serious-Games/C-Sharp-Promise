75fbb032dda00776a08bdbf6d6abf67638354b9c
Adam Single <adam@adamsingle.com>
2015-05-19 12:53:51 +1000

    PromiseTimer: Added initial test for the promise timer and the class.

---
+++ PromiseTimer.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RSG
+{
+    /// <summary>
+    /// A class that wraps a pending promise with it's predicate and time data
+    /// </summary>
+    internal class PredicateWait
+    {
+        /// <summary>
+        /// Predicate for resolving the promise
+        /// </summary>
+        public Func<TimeData, bool> predicate;
+
+        /// <summary>
+        /// The time the promise was started
+        /// </summary>
+        public float timeStarted;
+
+        /// <summary>
+        /// The pending promise which is an interface for a promise that can be rejected or resolved.
+        /// </summary>
+        public IPendingPromise pendingPromise;
+
+        /// <summary>
+        /// The time data specific to this pending promise. Includes elapsed time and delta time.
+        /// </summary>
+        public TimeData timeData;
+    }
+
+    /// <summary>
+    /// Time data specific to a particular pending promise.
+    /// </summary>
+    public struct TimeData
+    {
+        /// <summary>
+        /// The amount of time that has elapsed since the pending promise started running
+        /// </summary>
+        public float elapsedTime;
+
+        /// <summary>
+        /// The amount of time since the last time the pending promise was updated.
+        /// </summary>
+        public float deltaTime;
+    }
+
+    public interface IPromiseTimer
+    {
+        /// <summary>
+        /// Resolve the returned promise once the time has elapsed
+        /// </summary>
+        IPromise WaitFor(float seconds);
+
+        /// <summary>
+        /// Update all pending promises. Must be called for the promises to progress and resolve at all.
+        /// </summary>
+        void Update(float deltaTime);
+    }
+
+    public class PromiseTimer : IPromiseTimer
+    {
+        /// <summary>
+        /// The current running total for time that this PromiseTimer has run for
+        /// </summary>
+        private float curTime;
+
+        /// <summary>
+        /// Currently pending promises
+        /// </summary>
+        private List<PredicateWait> waiting = new List<PredicateWait>();
+
+        /// <summary>
+        /// Resolve the returned promise once the time has elapsed
+        /// </summary>
+        public IPromise WaitFor(float seconds)
+        {
+            return new Promise();
+        }
+
+        /// <summary>
+        /// Update all pending promises. Must be called for the promises to progress and resolve at all.
+        /// </summary>
+        public void Update(float deltaTime)
+        {
+            curTime += deltaTime;
+
+            int i = 0;
+            while(i < waiting.Count)
+            {
+                var wait = waiting[i];
+
+                var newElapsedTime = curTime - wait.timeStarted;
+                wait.timeData.deltaTime = newElapsedTime - wait.timeData.elapsedTime;
+                wait.timeData.elapsedTime = newElapsedTime;
+
+                if(wait.predicate(wait.timeData))
+                {
+                    wait.pendingPromise.Resolve();
+                    waiting.RemoveAt(i);
+                }
+                else
+                {
+                    i++;
+                }
+            }
+        }
+    }
+}
+
---
+++ Tests/PromiseTimerTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace RSG.Tests
+{
+    public class PromiseTimerTests
+    {
+        [Fact]
+        public void wait_for_doesnt_resolve_before_specified_time()
+        {
+            var promiseTimer = new PromiseTimer();
+            var testTime = 2f;
+            var hasResolved = false;
+
+            promiseTimer.WaitFor(testTime)
+                .Then(() => hasResolved = true)
+                .Done();
+
+            promiseTimer.Update(1f);
+
+            Assert.Equal(false, hasResolved);
+        }
+    }
+}
